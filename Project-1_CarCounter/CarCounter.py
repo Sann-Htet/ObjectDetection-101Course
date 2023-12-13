10c1277b9640efd9649ba11080ce7cab54f7a458
Sann-Htet <sannhtet899@gmail.com>
2023-12-14 03:26:26 +0630

    Counting cars by using opencv and YOLO detector and SORT tracker

--- Project-1_CarCounter/CarCounter.py
+++ Project-1_CarCounter/CarCounter.py
@@ -3,6 +3,7 @@ import cvzone
 import math
 import numpy as np
 from ultralytics import YOLO
+from sort import *
 
 cap = cv2.VideoCapture("ObjectDetectionYOLO/Videos/cars.mp4")
 
@@ -15,11 +16,21 @@ model = YOLO('ObjectDetectionYOLO/Yolo-weights/yolov8n.pt')
 
 mask = cv2.imread("Project-1_CarCounter/mask_resize.png")
 
+# Tracking
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+limits = [400, 297, 673, 297]
+totalCount = []
+
 while True:
     success, img = cap.read()
     imgRegion = cv2.bitwise_and(np.array(img), np.array(mask))
 
-    results = model(img, stream=True)
+    imgGraphics = cv2.imread("Project-1_CarCounter/graphics.png", cv2.IMREAD_UNCHANGED)
+    img = cvzone.overlayPNG(img, imgGraphics, (0, 0))
+
+    results = model(imgRegion, stream=True)
+    detections = np.empty((0, 5))
 
     for r in results:
         boxes = r.boxes
@@ -38,11 +49,35 @@ while True:
             cls = int(box.cls[0])
             currentClass = classNames[cls]
             if currentClass == "car" or currentClass == "truck" or currentClass == "bus" or currentClass == "motorbike" and conf > 0.3:
-                cvzone.cornerRect(img, (x1, y1, w, h), l=10)
-                cvzone.putTextRect(img, f'{currentClass} {conf}%', (max(0, x1), max(35, y1)), scale=1, thickness=1, offset=3)
+                # cvzone.cornerRect(img, (x1, y1, w, h), l=10, rt=5)
+                # cvzone.putTextRect(img, f'{currentClass} {conf}%', (max(0, x1), max(35, y1)), scale=1, thickness=1, offset=3)
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+
+    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
+
+    for result in resultsTracker:
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        w, h = x2 - x1, y2 - y1
+        cvzone.cornerRect(img, (x1, y1, w, h), l=10, rt=2, colorR=(255, 0, 255))
+        cvzone.putTextRect(img, f'{int(id)}', (max(0, x1), max(35, y1)), scale=2, thickness=3, offset=10)
+        cx, cy = x1 + w // 2, y1 + h // 2
+        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+        if limits[0] < cx < limits[2] and limits[1] - 15 < cy < limits[1] + 15:
+            if totalCount.count(id) == 0:
+                totalCount.append(id)
+                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
+            
+    # cvzone.putTextRect(img, f'Count: {len(totalCount)}', (50, 50))
+        
+    cv2.putText(img, str(len(totalCount)), (255, 100), cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 255), 8)
 
     cv2.imshow("Image", img)
-    cv2.imshow("Image Region", imgRegion)
+    # cv2.imshow("Image Region", imgRegion)
 
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
